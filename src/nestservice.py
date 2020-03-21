5f163f7f183f565733bb73b7ceb3df16e18d4c41
Juan Florez <j.florez-velasquez@mycit.ie>
2020-03-21 13:20:30 +0100

    Spawn a turtle progrmatically
    The trigger calls the the nestservice that will keep track of the turtles it creates

--- src/nestservice.py
+++ src/nestservice.py
@@ -2,15 +2,22 @@
 
 import rospy
 from std_srvs.srv import Empty
+from turtlesim.srv import Spawn, SpawnRequest, SpawnResponse
 
+rospy.wait_for_service('/spawn')
+serviceclient = rospy.ServiceProxy('/spawn', Spawn)
+turtlelist = []
+counter = 0
 
-def test_callback(empty):
+def turtle_creator(empty):
     print("Test called")
+    positionrequest = SpawnRequest(float(2), float(3), 0.6, (counter += 1).str() )
+    turtlelist.append(serviceclient(positionrequest))
     return []
 
 
 rospy.init_node('nest_server')
-rospy.Service('/nest_service', Empty, test_callback)
+rospy.Service('/nest_service', Empty, turtle_creator())
 
 rospy.spin()
 
