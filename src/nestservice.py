19a015286b221d0002f33c9cb28ee237e6d99122
Juan Florez <j.florez-velasquez@mycit.ie>
2020-03-21 13:59:12 +0100

    Spawn a multiple turtles progrmatically
    The trigger can be called multiple times

--- src/nestservice.py
+++ src/nestservice.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 
 import rospy
+import random
 from std_srvs.srv import Empty
 from turtlesim.srv import Spawn, SpawnRequest, SpawnResponse
+
 
 rospy.wait_for_service('/spawn')
 serviceclient = rospy.ServiceProxy('/spawn', Spawn)
 turtlelist = []
-counter = 0
+
 
 def turtle_creator(empty):
     print("Test called")
-    positionrequest = SpawnRequest(float(2), float(3), 0.6, (counter += 1).str() )
+    positionrequest = SpawnRequest(random.uniform(0,10), random.uniform(0,10), random.uniform(0,2),'post'+str(random.randint(0,23)))
     turtlelist.append(serviceclient(positionrequest))
     return []
 
 
 rospy.init_node('nest_server')
-rospy.Service('/nest_service', Empty, turtle_creator())
+rospy.Service('/nest_service', Empty, turtle_creator)
 
 rospy.spin()
 
