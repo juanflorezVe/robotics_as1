a5f5d1671309bb14089a4bd1c71828674724b9e8
juan florez <j.florez-velasquez@mycit.ie>
2020-07-26 10:13:48 +0200

    REBUILD CATKIN, skeleton for new service that returns the location of the nearest object

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -9,6 +9,7 @@ import rospy
 from std_msgs.msg import String
 from turtlesim.srv import Spawn, SpawnRequest, SpawnResponse
 from r00184264_a1.msg import location
+from r00184264_a1.srv import ClosestObj, ClosestObjResponse
 
 print "HELLO I AM DISPATCHER"
 rospy.loginfo("Dispatcher file activated")
@@ -26,13 +27,25 @@ def listen_new_turtles():
 
 def react_to_removed_turtle(data):
     rospy.loginfo("{} was removed from the floor".format(data.name))
+    #TODO: remove the turtle from the list
 
 def listen_removed_turtle():
     rospy.init_node("dispatcher")
     rospy.Subscriber("robot1_topic", location, react_to_removed_turtle)
-
+#===============================
 
 listen_new_turtles()
 listen_removed_turtle()
+#====================== Service to tell the next one ===============
+
+def get_closest_turtle(location):
+    '''
+    Using the manhattan algo, get the closest turtle in the floor to the location
+    '''
+    #TODO implement
+    return ClosestObjResponse()
+
+rospy.init_node("dispatcher")
+rospy.Service('/next_closest_turtle', ClosestObj, get_closest_turtle)
 
 rospy.spin()
--- src/picker.py
+++ src/picker.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 from std_srvs.srv import Empty, EmptyRequest
 from r00184264_a1.srv import TurtlesAmount, TurtlesAmountRequest
+from r00184264_a1.srv import ClosestObj, ClosestObjRequest
 
 
 import rospy
 import time
 
+
 #After created, wait 3 secs and delete one
 time.sleep(3)
+
+#Ask the dispatcher what is the closest turtle to the current position.
+print("I WILL START COLLECTING SOON")
 rospy.init_node('picker')
+rospy.wait_for_service('/next_closest_turtle')
+#TODO: calculate starting position
+nextlocationrequest = ClosestObjRequest()
+rospy.loginfo('passed 456')
+
+
+
 rospy.wait_for_service('/pickup_service')
 pickuprequest = TurtlesAmountRequest()
 pickuprequest.amount = 1
